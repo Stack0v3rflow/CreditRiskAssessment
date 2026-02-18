eee6b6b954c1124fc226f9505bb794e3d4f6a561
Silvio <younggravity0@icloud.com>
2026-02-18 20:54:34 +0100

    Implementierung von Backtracking und Optimierung der Parameter

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import numpy as np
 dataSetSize = 1000
 class Bias(object):
     def __init__(self) :
-        self.w_Income = np.random.randn()
-        self.w_Age = np.random.randn()
-        self.w_Credits = np.random.randn()
-
+        self.w_Income = 0.5
+        self.w_Age = 0.5
+        self.w_Credits = 0.5
+        self.Bias = 0.5
 
 
 class User(object):
@@ -32,27 +32,17 @@ class User(object):
             print("Number of credits cant be less than 0")
             return False
 
-    def defaultIncome(self):
-        tempIncome = (self.income - self.d_income) / self.sigma_income
-        return tempIncome
-    def defaultAge(self):
-        tempAge = (self.age - self.d_age) / self.sigma_age
-        return tempAge
-    def defaultCredits(self):
-        tempCredits = (self.b_credits - self.d_credits) / self.sigma_credits
-        return tempCredits
+    def get_inputs(self):
+        x1 = (self.income - self.d_income) / self.sigma_income
+        x2 = (self.age - self.d_age) / self.sigma_age
+        x3 = (self.b_credits - self.d_credits) / self.sigma_credits
+        return np.array([x1, x2, x3])
 
-    def logisticLog(self, expectedBias : Bias):
-        z = self.defaultIncome() * expectedBias.w_Income + self.defaultAge() * expectedBias.w_Age + self.defaultCredits() * expectedBias.w_Credits
-        return 1/ (1+ np.exp( -z ))
+def sigmoid(z):
+    return 1/ (1+ np.exp( -z ))
+def sigmoid_deriv(z):
+    return z*(1-z)
 
-    def lossFunction(self, expectedBias : Bias):
-        if self.credit_default == 1 :
-            loss = np.log(self.logisticLog(expectedBias))
-            return loss
-        if  self.credit_default == 0 :
-            loss = (np.log(self.logisticLog(expectedBias)) ) * -1
-            return loss
 
 
 
@@ -67,5 +57,35 @@ for i in range (dataSetSize):
 newBias = Bias()
 for user in userlist:
     user.verify()
-for user in userlist:
-    print("Chance eines defaults:" , user.logisticLog(newBias))
+
+learning_rate = 0.1
+
+for epoch in range(100):
+    for user in userlist:
+        # 1. Inputs holen
+        x = user.get_inputs()  # [x1, x2, x3]
+        target = user.credit_default
+
+        # 2. Forward Pass (Vorhersage)
+        # z = w1*x1 + w2*x2 + w3*x3 + b
+        z = (x[0] * newBias.w_Income +
+             x[1] * newBias.w_Age +
+             x[2] * newBias.w_Credits +
+             newBias.Bias)
+        prediction = sigmoid(z)
+
+        # 3. Backpropagation (Fehler berechnen)
+        error = target - prediction
+
+        # Der Gradient (Fehler * Steigung der Sigmoid-Kurve)
+        d_z = error * sigmoid_deriv(prediction)
+
+        # 4. Update der Gewichte (Schuldzuweisung)
+        # Jedes Gewicht wird anteilig an seinem Input korrigiert
+        newBias.w_Income += learning_rate * d_z * x[0]
+        newBias.w_Age += learning_rate * d_z * x[1]
+        newBias.w_Credits += learning_rate * d_z * x[2]
+        newBias.Bias += learning_rate * d_z
+
+# Test-Ausgabe
+print(f"Neue Gewichte nach Training: Income: {newBias.w_Income:.2f}, Age: {newBias.w_Age:.2f}")
